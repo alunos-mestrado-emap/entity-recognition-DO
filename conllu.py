106a6e91a13d3d8b0fd54b1ec4c8d98ed4e77caf
odanoburu <bcclaro@gmail.com>
2017-09-11 16:21:06 -0300

    add conllu files with IOB tags

--- conllu.py
+++ conllu.py
@@ -1,14 +1,25 @@
+"""
+this is a hacky script to read CoNLL-U format files and output them in 
+the format needed for NER.
+
+if you are using the bosque sintá(c)tico version, where some multiword tokens are marked with '=' (e.g., além=de), running:
+
+cat transfbosque_CP.udep.conll | sed 's/=/\t0\n/' > transfCP.conllu
+
+should solve your problem.
+
+"""
+
 import csv
 import os
-import itertools
 
-def should_print(element):
+def should_print(element, ix=None):
     if element == 'PROPN':
         return element
     else:
         return '0'
 
-def conllu_transform(filepath):
+def ownpt_conllu_transform(filepath):
     if not os.path.isfile(filepath):
         raise Exception("not file.")
     dirpath, filename = os.path.split(filepath)
@@ -29,14 +40,39 @@ def conllu_transform(filepath):
                     for i in range(start, end + 1):
                         next(tsvin)
 
-def dir_conllu_transform(dirpath):
-    directory = os.fsencode(dirpath)
-    if not os.path.isdir(directory):
+def ud_should_print(element, ix=None):
+    if element == 'PROPN' and ix == 0:
+        return 'B-' + element
+    elif element == 'PROPN':
+        return 'I-' + element
+    else:
+        return '0'
+
+def ud_conllu_transform(filepath):
+    if not os.path.isfile(filepath):
+        raise Exception("not file.")
+    dirpath, filename = os.path.split(filepath)
+    output_path = os.path.join(dirpath, 'transf'+filename)
+    with open(filepath,'r') as tsvin, open(output_path, 'w') as tsvout:
+        tsvin = csv.reader(tsvin, delimiter='\t')
+        tsvout = csv.writer(tsvout, delimiter='\t')
+        for row in tsvin:
+            if not row:
+                tsvout.writerow(row)
+            # several mistakes in ud files which cause newlines where there shouldn't be one
+            elif row[0][0] not in '0123456789':
+                continue
+            else:
+                tokens = row[1].split('_')
+                for ix, token in enumerate(tokens):
+                    tsvout.writerow([token, ud_should_print(row[3], ix)])
+
+def dir_conllu_transform(dirpath, transf_function):
+    if not os.path.isdir(dirpath):
         raise Exception('must be directory.')
-    for file in os.listdir(directory):
-        filename = os.fsdecode(file)
-        if filename.endswith(".conllu"): 
-            conllu_transform(filename)
+    for file in os.scandir(dirpath):
+        if file.name.endswith(".conllu") or file.name.endswith(".conll"):
+            transf_function(file.path)
         else:
             continue
                     
